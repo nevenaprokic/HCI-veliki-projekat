1cd5a349fffe75910960f4577f61a50efd9fa18b
Nevena Prokic <prokic.sw6.2019@uns.ac.rs>
2022-05-31 18:13:52 +0200

    Promena menu bara u nav bar.

---
+++ SyncfusionWpfApp1/gui/NetworkTrainLineMenager.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SyncfusionWpfApp1.gui
+{
+    public partial class NetworkTrainLine : Page
+    {
+        private Frame frame;
+        Point startPoint = new Point();
+        public NetworkTrainLine(Frame f)
+        {
+            InitializeComponent();
+            frame = f;
+            Uri iconUriMail = new Uri("../../../images/proba.png", UriKind.RelativeOrAbsolute);
+            logoIcon.Source = BitmapFrame.Create(iconUriMail);
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri("../../../images/ReservationBackground.png", UriKind.Relative));
+            this.Background = myBrush;
+        }
+        private void ListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                Image image = e.Source as Image;
+                DataObject data = new DataObject(typeof(ImageSource), image.Source);
+                DragDrop.DoDragDrop(image, data, DragDropEffects.Move);
+            }
+        }
+        private void ListView_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") || sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void ListView_Drop(object sender, DragEventArgs e)
+        {
+           /* Image imageControl = (Image)sender;
+            if ((e.Data.GetData(typeof(ImageSource)) != null))
+            {
+                img1.Source = (ImageSource)e.Data.GetData(typeof(ImageSource));
+                img1 = imageControl;
+            }*/
+            /*if (e.Data.GetDataPresent("myFormat"))
+            {
+                Console.WriteLine(e.Data.GetData("myFormat"));
+            }*/
+        }
+        
+
+        private void TicketReport_Handler(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void TicketReservation_Handler(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void MonthlyReport_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new MonthlyReport(frame);
+        }
+        private void TrainLineReport_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new TrainLineReport(frame);
+        }
+        private void Schedule_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new ScheduleUpdateDelete(frame);
+        }
+        private void NetworkTrainLine_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new NetworkTrainLine(frame);
+        }
+        private void TrainLine_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new TrainLineCRUD(frame);
+        }
+        private void Train_Handler(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new TrainUpdateDelete(frame);
+        }
+    }
+}
