0fce89af40fbfb96518c6e4f1059d6446b901087
jovancevic <jovancevic.sw1.2019@uns.ac.rs>
2022-06-06 13:41:36 +0200

    na create schedule je dodat video player

---
+++ SyncfusionWpfApp1/gui/MediaElement.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SyncfusionWpfApp1.gui
+{
+    /// <summary>
+    /// Interaction logic for MediaElement.xaml
+    /// </summary>
+    public partial class MediaElement : Window
+    {
+        public MediaElement()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+			mePlayer.Source = new Uri("../../../images/video.mkv", UriKind.Relative);
+			mePlayer.LoadedBehavior = MediaState.Manual;
+        }
+
+		void timer_Tick(object sender, EventArgs e)
+		{
+			if (mePlayer.Source != null)
+			{
+				if (mePlayer.NaturalDuration.HasTimeSpan)
+					lblStatus.Content = String.Format("{0} / {1}", mePlayer.Position.ToString(@"mm\:ss"), mePlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+			}
+			else
+				lblStatus.Content = "No file selected...";
+		}
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			mePlayer.Play();
+			labela.Visibility = Visibility.Hidden;
+		}
+
+		private void btnPause_Click(object sender, RoutedEventArgs e)
+		{
+			mePlayer.Pause();
+		}
+
+		private void btnStop_Click(object sender, RoutedEventArgs e)
+		{
+			mePlayer.Stop();
+		}
+	}
+}
